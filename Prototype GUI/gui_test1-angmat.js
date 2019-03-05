8e2cd052ff1785c92e88c9e5b7bd049ed278545e
JosephPellegrino219 <sumgun456@gmail.com>
2019-03-05 17:01:51 -0500

    Added Prototype GUI files

---
+++ Prototype GUI/gui_test1-angmat.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+
+  angular.module('MapApp', ['ngMaterial'])
+    .controller('AppCtrl', function($scope) {
+      var app = this;
+
+      /* Call leaflet function (potentially create a Map obj instead) */
+      var usMap = new Map();
+      usMap.mapSetup();
+
+      app.isOpen = false;
+      app.hover = false;
+
+      app.selectedMode = 'md-scale';
+
+      app.selectedDirection = 'left';
+
+      /* Defining menu item info */
+      app.items = [
+        { name: "Batch Run", icon: "img/icons/baseline-add_photo_alternate-24px.svg", direction: "bottom" },
+        { name: "Single Run", icon: "img/icons/baseline-photo-24px.svg", direction: "bottom" }
+      ];
+
+      /* Handling application menu */
+      app.buttonIcons = {
+      	prev: { name: "Previous State", icon: "img/icons/baseline-navigate_before-24px.svg"},
+      	next: { name: "Next State", icon: "img/icons/baseline-navigate_next-24px.svg"}
+      };
+      app.states=usMap.leaf_states;
+
+      $scope.menuToggle = {};
+    });
+})();
